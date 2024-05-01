e1c9e76300ab6ceea0b98fa3ff1b1f5bae17bb5c
syeef <syeefislam@yahoo.com>
2024-05-01 21:23:06 +0530

    incerement by 2 and decrement by 2 done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,30 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "incrementby2" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrementby2" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
