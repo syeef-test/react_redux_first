b8d7f710f2403cb40f8bd7c36d53ab71f6cbb5e4
syeef <syeefislam@yahoo.com>
2024-05-02 21:13:26 +0530

    logout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 import "./App.css";
 import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
-      <Counter />
+      <Fragment>
+        <Header />
+        {!isAuth && <Auth />}
+        {isAuth && <UserProfile />}
+        <Counter />
+      </Fragment>
     </>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ import { counterActions } from "../store";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
